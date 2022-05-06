373b938499494bb35d6cde12ccb5cfbdf100fc60
Vince <vincent.tran.ece@gmail.com>
2022-05-05 22:40:50 -0500

    adds and styles react-select dropdown with autosuggest

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { Component, useState } from 'react';
 import VideoPlayer from './VideoPlayer.jsx';
 import EventsList from './EventsList.jsx';
+import Select from 'react-select';
 
 const Home = ({ handleCity, currentVideoInfo, eventsCollection, setCurrentVideoInfo }) => {
+
+  const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' }
+  ];
+
+  const customStyles = {
+    option: provided => ({
+      color: 'black'
+    })
+  };
+
+  const [selectedOption, setSelectedOption] = useState(null)
 
   return (
     <div className="container">
       <section className="body-content-container">
         <div className="user-city">
-          <form className="search-form" onSubmit={(e) => { handleCity(e) }}>
+          <form className="search-form">
             <label className="text-spacing" htmlFor="city-zip">City/Zip</label>
-            <input className="input-field" type="text" autoComplete="home city" placeholder="city or zip code" />
-            <input className="btn-primary" type="submit" />
+            <Select className="input-field" styles={customStyles} value={selectedOption} onChange={(cityId)=>
+              {setSelectedOption(cityId);
+              handleCity(cityId);
+              }} options={options} />
           </form>
         </div>
         <VideoPlayer currentVideoInfo={currentVideoInfo} />
@@ -27,3 +44,22 @@ const Home = ({ handleCity, currentVideoInfo, eventsCollection, setCurrentVideoI
 
 export default Home;
 
+// return (
+//   <div className="container">
+//     <section className="body-content-container">
+//       <div className="user-city">
+//         <form className="search-form" onSubmit={(e) => { handleCity(e) }}>
+//           <label className="text-spacing" htmlFor="city-zip">City/Zip</label>
+//           <input className="input-field" type="text" autoComplete="home city" placeholder="city or zip code" />
+//           <input className="btn-primary" type="submit" />
+//         </form>
+//       </div>
+//       <VideoPlayer currentVideoInfo={currentVideoInfo} />
+//     </section>
+//     <section>
+//       <div className="events-container">
+//         <EventsList eventsCollection={eventsCollection} handleVideoSelect={setCurrentVideoInfo} />
+//       </div>
+//     </section>
+//   </div>
+// )
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Switch, Route, Link, Routes } from "react-router-dom";
-import VideoPlayer from './components/VideoPlayer.jsx';
-import EventsList from './components/EventsList.jsx';
 import FavoriteArtists from './components/FavoriteArtists.jsx';
 import Navbar from './components/Navbar.jsx';
 import Home from './components/Home.jsx';
@@ -17,32 +15,30 @@ function App() {
 
 
   function handleCity(e) {
-    e.preventDefault();
 
-    getEvents('08401')
-      .then(({ data }) => {
-        setEventsCollection(data);
-        // console.log(data);
-      })
-      .catch((err) => {
-        console.log(err.message);
-      });
+      console.log('THIS IS e: ', e.value)
+    // getEvents('08401')
+    //   .then(({ data }) => {
+    //     setEventsCollection(data);
+    //     // console.log(data);
+    //   })
+    //   .catch((err) => {
+    //     console.log(err.message);
+    //   });
   }
 
   useEffect(() => {
-    if (!isLoaded) {
-      getEvents('08401')
-        .then(({ data }) => {
-          setEventsCollection(data);
-          // console.log(data);
-        })
-        .catch((err) => {
-          console.log(err.message);
-        });
-        setIsLoaded(true);
-    }
-
-    console.log('RAN')
+    // if (!isLoaded) {
+    //   getEvents('08401')
+    //     .then(({ data }) => {
+    //       setEventsCollection(data);
+    //       // console.log(data);
+    //     })
+    //     .catch((err) => {
+    //       console.log(err.message);
+    //     });
+    //     setIsLoaded(true);
+    // }
   })
 
   return (
@@ -52,23 +48,8 @@ function App() {
           <Navbar />
           <Routes>
             <Route exact path="/" element={
-              <div className="container">
-                <section className="body-content-container">
-                  <div className="user-city">
-                    <form className="search-form" onSubmit={(e) => { handleCity(e) }}>
-                      <label className="text-spacing" htmlFor="city-zip">City/Zip</label>
-                      <input className="input-field" type="text" autoComplete="home city" placeholder="city or zip code" />
-                      <input className="btn-primary" type="submit" />
-                    </form>
-                  </div>
-                  <VideoPlayer currentVideoInfo={currentVideoInfo} />
-                </section>
-                <section>
-                  <div className="events-container">
-                    <EventsList eventsCollection={eventsCollection} handleVideoSelect={setCurrentVideoInfo} />
-                  </div>
-                </section>
-              </div>
+              <Home handleCity={handleCity} currentVideoInfo={currentVideoInfo}
+              eventsCollection={eventsCollection} setCurrentVideoInfo={setCurrentVideoInfo}/>
             } />
             <Route exact path="favoriteArtists" element={
               <FavoriteArtists />
